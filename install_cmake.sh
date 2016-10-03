3dd141738bb3672fe0d485a9a4c601cd67e324be
Jorge M <deuxbot@users.noreply.github.com>
2016-10-03 14:07:58 +0200

    Add files via upload

---
+++ install_cmake.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [ "$#" -ne 1 ]; then
+	echo "Usage ./install_cmake.sh installPath"
+	exit
+fi
+DEST_PATH=$1
+FILE_NAME=cmake-3.6.2
+wget https://cmake.org/files/v3.6/$FILE_NAME.tar.gz
+tar -xzvf $FILE_NAME.tar.gz 
+rm $FILE_NAME.tar.gz
+cd $FILE_NAME
+./configure --prefix=$DEST_PATH
+make
+make check
+make install
+rm -rf ../$FILE_NAME
