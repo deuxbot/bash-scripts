a5549b7b9955ba029778e5714fa8feecd74b0ce3
Jorge M <deuxbot@users.noreply.github.com>
2016-10-03 14:09:29 +0200

    Update install_gsl.sh

--- install_gsl.sh
+++ install_gsl.sh
@@ -4,14 +4,15 @@ if [ "$#" -ne 1 ]; then
 	exit
 fi
 DEST_PATH=$1
-wget ftp://ftp.gnu.org/gnu/gsl/gsl-2.1.tar.gz 
-tar -xzvf gsl-2.1.tar.gz 
-rm gsl-2.1.tar.gz
-cd gsl-2.1
+FILE_NAME=gsl-2.1
+wget ftp://ftp.gnu.org/gnu/gsl/$FILE_NAME.tar.gz 
+tar -xzvf $FILE_NAME.tar.gz 
+rm $FILE_NAME.tar.gz
+cd $FILE_NAME
 ./configure --prefix=$DEST_PATH
 make
 make check
 make install
 echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$DEST_PATH/lib' >> ~/.bashrc 
 echo 'export LIBRARY_PATH=$LIBRARY_PATH:$DEST_PATH/lib' >> ~/.bashrc 
-rm -rf ../gsl-2.1
+rm -rf ../$FILE_NAME
